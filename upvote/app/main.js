f9ac824df057076066d105add34ba479e1a6986e
sorxcode <sorxcode@gmail.com>
2021-03-07 06:45:14 +0100

    completed upvote

--- upvote/app/main.js
+++ upvote/app/main.js
@@ -1,3 +1,50 @@
+const submissionComponent = {
+    template: `
+    <div style="display: flex; width: 100%">
+        <figure class="media-left">
+                <img
+                class="image is-64x64"
+                v-bind:src="submission.submissionImage"
+                />
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                <p>
+                    <strong>
+                    <a v-bind:href="submission.url" class="has-text-info"
+                        >{{ submission.title }}</a
+                    >
+                    <span class="tag is-small">#{{ submission.id }}</span>
+                    </strong>
+                    <br />
+                    {{ submission.description }}
+                    <br />
+                    <small class="is-size-7">
+                    Submitted by:
+                    <img
+                        class="image is-24x24"
+                        v-bind:src="submission.avatar"
+                    />
+                    </small>
+                </p>
+                </div>
+            </div>
+            <div class="media-right">
+                <span v-on:click="upvote(submission.id)" class="icon is-small">
+                <i class="fa fa-chevron-up"></i>
+                <strong class="has-text-info">{{ submission.votes }}</strong>
+                </span>
+            </div>
+    </div>`,
+    props: ["submission", "submissions"],
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find((submission) => submission.id === submissionId);
+            submission.votes++
+        }
+    },
+}
+
 const upvoteApp = {
     data() {
         return { submissions: Seed.submissions }
@@ -9,11 +56,8 @@ const upvoteApp = {
             })
         }
     },
-    methods: {
-        upvote(submissionId) {
-            const submission = this.submissions.find((submission) => submission.id === submissionId);
-            submission.votes++
-        }
+    components: {
+        "submission_component": submissionComponent,
     }
 };
 
